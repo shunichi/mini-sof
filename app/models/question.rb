eeed992251ac81751c51d91dd1d09424a51f7e16
Shunichi Ikegami <sike.tm@gmail.com>
2014-09-06 16:05:34 +0900

    Add Question model validation and spec.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :body, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "ユーザーIDとタイトルと本文が設定されていれば有効" do
+    question = FactoryGirl.build(:question)
+    expect(question).to be_valid
+  end
+
+  it "ユーザーが指定されいていないと無効" do
+    question = FactoryGirl.build(:question, user_id: nil)
+    expect(question).to have(1).errors_on(:user_id)
+  end
+
+  it "タイトルが空だと無効" do
+    question = FactoryGirl.build(:question, title:'')
+    expect(question).to have(1).errors_on(:title)
+  end
+
+  it "本文が空だと無効" do
+    question = FactoryGirl.build(:question, body:'')
+    expect(question).to have(1).errors_on(:body)
+  end
+
+  it "タイトルが長すぎると無効" do
+    question = FactoryGirl.build(:question, title: 'a' * 251)
+    expect(question).to have(1).errors_on(:title)
+  end
 end
