ebc6eacb745d1b30bf22ac451cc10478a8fd6526
Shunichi Ikegami <sike.tm@gmail.com>
2014-09-23 10:55:21 +0900

    Add a div surrounding question to question#show view

--- spec/features/votes_spec.rb
+++ spec/features/votes_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature '投票' do
   given(:user) { FactoryGirl.create(:user) }
   given(:question) { FactoryGirl.create(:question, user: user) }
+  given!(:answer) { FactoryGirl.create(:answer, question: question)}
 
   feature 'ログインしている場合' do
     background do
@@ -12,42 +13,42 @@ feature '投票' do
 
     scenario 'upvote すると票数が増える', js: true do
       expect {
-        find('.vote-arrow.upvote').click
+        find('.question').find('.vote-arrow.upvote').click
         wait_for_ajax
       }.to change { question.vote_sum }.by(1)
     end
 
     scenario 'downvote すると票数が減る', js: true do
       expect {
-        find('.vote-arrow.downvote').click
+        find('.question').find('.vote-arrow.downvote').click
         wait_for_ajax
       }.to change { question.vote_sum }.by(-1)
     end
 
     scenario 'vote を逆に変更できる', js: true do
-        find('.vote-arrow.upvote').click
+        find('.question').find('.vote-arrow.upvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq 1
-        find('.vote-arrow.downvote').click
+        find('.question').find('.vote-arrow.downvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq -1
-        find('.vote-arrow.upvote').click
+        find('.question').find('.vote-arrow.upvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq 1
     end
 
     scenario 'vote の取り消しができる', js: true do
-        find('.vote-arrow.upvote').click
+        find('.question').find('.vote-arrow.upvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq 1
-        find('.vote-arrow.upvote').click
+        find('.question').find('.vote-arrow.upvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq 0
 
-        find('.vote-arrow.downvote').click
+        find('.question').find('.vote-arrow.downvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq -1
-        find('.vote-arrow.downvote').click
+        find('.question').find('.vote-arrow.downvote').click
         wait_for_ajax
         expect(question.vote_sum).to eq 0
     end
@@ -59,7 +60,7 @@ feature '投票' do
       expect(page).to_not have_css('.vote-arrow.upvote')
       expect(page).to_not have_css('.vote-arrow.downvote')
       expect(page).to have_css('.vote-arrow.disabled')
-      arrows = all('.vote-arrow')
+      arrows = find('.question').all('.vote-arrow')
       expect(arrows.size).to eq 2
       arrows.each do |element|
         element.click
